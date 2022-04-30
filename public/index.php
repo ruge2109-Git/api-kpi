2628ce1cabe9b897b38f42e4e0a81dfe664f5cef
Jonathan Ruge <53587500+ruge2109-Git@users.noreply.github.com>
2022-04-30 01:16:00 -0500

    feature: Procfile

--- public/index.php
+++ public/index.php
@@ -11,19 +11,17 @@ $app = new \Slim\App;
 $app->get('/',function(Request $request,Response $response){
     echo "HOLA API";
 });
-$app->get('/api',function(Request $request,Response $response){
-    echo "HOLA API 2";
-});
+
 // RUTA PRODUCTOS
-// require '../src/rutas/productos.php';
-// require '../src/rutas/tienda_online.php';
+require '../src/rutas/productos.php';
+require '../src/rutas/tienda_online.php';
 
 
-// $app->add(function ($req, $res, $next) {
-//     $response = $next($req, $res);
-//     return $response
-//         ->withHeader('Access-Control-Allow-Origin', '*')
-//         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-//         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-// });
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 $app->run();
