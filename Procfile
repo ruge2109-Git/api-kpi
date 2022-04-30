c95d4bb3d5db6e093c3e7e1f059c9f1bfa7da46f
Jonathan Ruge <53587500+ruge2109-Git@users.noreply.github.com>
2022-04-30 01:14:04 -0500

    feature: Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: heroku-php-nginx public/
+web: vendor/bin/heroku-php-apache2 public/
--- public/index.php
+++ public/index.php
@@ -3,8 +3,8 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-require '../vendor/autoload.php';
-require '../src/config/db.php';
+require __DIR__.'../vendor/autoload.php';
+require __DIR__.'../src/config/db.php';
 
 $app = new \Slim\App;
 
