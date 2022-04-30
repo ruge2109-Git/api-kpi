51c78cd9b595cd95fd8e2b83d5b1468d9de04c2c
Jonathan Ruge <53587500+ruge2109-Git@users.noreply.github.com>
2022-04-30 01:03:56 -0500

    feature: Procfile

--- public/index.php
+++ public/index.php
@@ -1,24 +1,24 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+// use Psr\Http\Message\ResponseInterface as Response;
+// use Psr\Http\Message\ServerRequestInterface as Request;
 
-require '../vendor/autoload.php';
-require '../src/config/db.php';
+// require '../vendor/autoload.php';
+// require '../src/config/db.php';
 
-$app = new \Slim\App;
+// $app = new \Slim\App;
 
-// RUTA PRODUCTOS
-require '../src/rutas/productos.php';
-require '../src/rutas/tienda_online.php';
+// // RUTA PRODUCTOS
+// require '../src/rutas/productos.php';
+// require '../src/rutas/tienda_online.php';
 
 
-$app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
-    return $response
-        ->withHeader('Access-Control-Allow-Origin', '*')
-        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-});
-$app->run();
+// $app->add(function ($req, $res, $next) {
+//     $response = $next($req, $res);
+//     return $response
+//         ->withHeader('Access-Control-Allow-Origin', '*')
+//         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+//         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+// });
+// $app->run();
 echo "HOLA MUNDOI";
