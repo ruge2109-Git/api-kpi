c4eef6d485a1a5ea4ac6de37d78f3cdd22aa7f8b
Jonathan Ruge <53587500+ruge2109-Git@users.noreply.github.com>
2022-04-30 00:40:32 -0500

    feature: Actualización db

--- src/config/db.php
+++ src/config/db.php
@@ -2,10 +2,14 @@
 
 class db
 {
-    private $dbHost = "localhost";
-    private $dbUser = "root";
-    private $dbPass = "";
-    private $dbName = "kpi";
+    // private $dbHost = "localhost";
+    // private $dbUser = "root";
+    // private $dbPass = "";
+    // private $dbName = "kpi";mysql://b071e4e043800c:42ca7c28@us-cdbr-east-05.cleardb.net/heroku_979c9eb1a0972f3?reconnect=true
+    private $dbHost = "us-cdbr-east-05.cleardb.net";
+    private $dbUser = "b071e4e043800c";
+    private $dbPass = "42ca7c28";
+    private $dbName = "heroku_979c9eb1a0972f3";
 
     public function connectDB(){
         $mysqlConnect = "mysql:host=$this->dbHost;dbname=$this->dbName";
